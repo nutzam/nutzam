9bc4f759372215e48f64b9331c1f1e4e4795be20
pangwu86 <pangwu86@gmail.com>
2017-01-16 22:42:06 +0800

    update:文档页尾添加导航，方便快速跳转到前后两篇文章

--- pages2/_tmpl/page.ftl
+++ pages2/_tmpl/page.ftl
@@ -9,12 +9,12 @@
     <script src="${doc.bpath}js/jquery.min.js"></script>
     <script src="${doc.bpath}js/js.cookie.min.js"></script>
     <#include "lib:theme.ftl">
-    <link rel="stylesheet" type="text/css" href="${doc.bpath}css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="${doc.bpath}css/balloon.min.css">
-    <link rel="stylesheet" type="text/css" href="${doc.bpath}css/bootstrap-switch.min.css">
-    <link rel="stylesheet" type="text/css" href="${doc.bpath}css/prism.css">
-    <script src="${doc.bpath}js/jquery.toTop.min.js"></script>
-</head>
+        <link rel="stylesheet" type="text/css" href="${doc.bpath}css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="${doc.bpath}css/balloon.min.css">
+        <link rel="stylesheet" type="text/css" href="${doc.bpath}css/bootstrap-switch.min.css">
+        <link rel="stylesheet" type="text/css" href="${doc.bpath}css/prism.css">
+        <script src="${doc.bpath}js/jquery.toTop.min.js"></script>
+        </head>
 <body tree-depth="${page.treeDepth}"
       tree-name="${page.treeName}"
       page-depth="${page.depth}" data-spy="scroll" data-target="#pagedoc-nav">
@@ -37,13 +37,17 @@
                 <#include "lib:docinfo.ftl">
                     <div class="doc-content">${doc.content}</div>
             </div>
+            <div class="doc-nav">
+                <div class="doc-nav-btn doc-nav-left "><i class="fa fa-arrow-left"></i><span></span></div>
+                <div class="doc-nav-btn doc-nav-right"><span></span><i class="fa fa-arrow-right"></i></div>
+            </div>
         </div>
-    <#include "lib:footer.ftl">
-    <script src="${doc.bpath}js/tether.min.js"></script>
-    <script src="${doc.bpath}js/bootstrap.min.js"></script>
-    <script src="${doc.bpath}js/bootstrap-switch.min.js"></script>
-    <script src="${doc.bpath}js/prism.js"></script>
-    <script src="${doc.bpath}js/nutzam-doc.js"></script>
+        <#include "lib:footer.ftl">
+            <script src="${doc.bpath}js/tether.min.js"></script>
+            <script src="${doc.bpath}js/bootstrap.min.js"></script>
+            <script src="${doc.bpath}js/bootstrap-switch.min.js"></script>
+            <script src="${doc.bpath}js/prism.js"></script>
+            <script src="${doc.bpath}js/nutzam-doc.js"></script>
 </div>
 </body>
 </html>
--- pages2/js/nutzam-doc.js
+++ pages2/js/nutzam-doc.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
 
 
     // 加载左边侧边栏
-
     var treeDepth = $body.attr('tree-depth') * 1;
     var pageDepth = $body.attr('page-depth') * 1;
     var treeName = $body.attr('tree-name');
@@ -26,6 +25,60 @@ $(document).ready(function () {
                 $(this).parents(".zdoc-index-node").removeClass("hidesub");
             }
         });
+
+
+        // 加载底部导航
+        var $nleft = $('.doc-nav-left');
+        var $nright = $('.doc-nav-right');
+        var $ca = $sidetree.find('a.current');
+        var $cli = $ca.parent();
+
+        // nav-prev
+        var $preva = null;
+        var $prevli = $cli.prev();
+        if ($prevli == null || $prevli.length == 0) {
+            var $prevnode = $cli.parent().parent(); // li.zdoc-index-node
+            if ($prevnode.hasClass('zdoc-index-node')) {
+                var $prevnode = $prevnode.prev();
+                if ($prevnode != null && $prevnode.length > 0) {
+                    $prevli = $prevnode.find('ol li:last-child');
+                }
+            }
+        }
+        $preva = $prevli.find('a');
+        if ($preva != null && $preva.length > 0) {
+            $nleft.find('span').text($preva.text());
+            $nleft.on('click', function () {
+                $preva.append('<span></span>');
+                $preva.find('span').click();
+            });
+        } else {
+            $nleft.remove();
+        }
+
+        // nav-next
+        var $nexta = null;
+        var $nextli = $cli.next();
+        if ($nextli == null || $nextli.length == 0) {
+            var $nextnode = $cli.parent().parent(); // li.zdoc-index-node
+            if ($nextnode.hasClass('zdoc-index-node')) {
+                var $nextnode = $nextnode.next();
+                if ($nextnode != null && $nextnode.length > 0) {
+                    $nextli = $nextnode.find('ol li:first-child');
+                }
+            }
+        }
+        $nexta = $nextli.find('a');
+        if ($nexta != null && $nexta.length > 0) {
+            $nright.find('span').text($nexta.text());
+            $nright.on('click', function () {
+                $nexta.append('<span></span>');
+                $nexta.find('span').click();
+            });
+        } else {
+            $nright.remove();
+        }
+
     }).delegate('.zdoc-index-node b', 'click', function () {
         $(this).parent().toggleClass('hidesub');
     });
